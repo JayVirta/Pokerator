aa1042ad792d3388f61920267079164fbd5b1a18
JayVirta <31388873+JayVirta@users.noreply.github.com>
2018-02-27 15:54:22 -0500

    Hand definitions

--- src/application/Hand.java
+++ src/application/Hand.java
@@ -9,14 +9,15 @@ Card card3;
 Card card4;
 Card card5;
 int handValue;
-private boolean pairFound;
-private boolean twoPair;
-private boolean threeOfKind;
-private boolean fourOfKind;
-private boolean Straight;
-private boolean Flush;
-private boolean fullHouse;
-private boolean royal;
+boolean handDetermined = false;
+private boolean pairFound= false;
+private boolean twoPair= false;
+private boolean threeOfKind= false;
+private boolean fourOfKind= false;
+private boolean Straight= false;
+private boolean Flush= false;
+private boolean fullHouse= false;
+private boolean royal= false;
 
 
 	public Hand(Card card1, Card card2, Card card3,Card card4, Card card5)
@@ -28,7 +29,7 @@ private boolean royal;
 		this.card5 = card5;
 	}
 	
-	public String handDetermine()
+	public void handDetermine()
 	{
 		ArrayList<Card> sortedHand = new ArrayList<Card>();
 		sortedHand.add(card1);
@@ -42,44 +43,129 @@ private boolean royal;
 		System.out.println(sortedHand.get(2).getValue());
 		System.out.println(sortedHand.get(3).getValue());
 		System.out.println(sortedHand.get(4).getValue());
-		
-		return "";
+		int[] values = new int[] {sortedHand.get(0).getValue(), sortedHand.get(1).getValue(), sortedHand.get(2).getValue(), sortedHand.get(3).getValue(),sortedHand.get(4).getValue()};
 	}
-	private boolean isPair()
+	private boolean isPair(int[] values)
 	{
-		return pairFound;
+		boolean check = false;
+		if(values[1] == values[2])
+			check = true;
+		if(values[2] == values[3])
+			check = true;
+		if(values[3] == values[4])
+			check = true;
+		if(values[4] == values[5])
+			check = true;
+		return check;
 	}
-	private boolean isTwoPair()
+	private boolean isTwoPair(int[] values)
 	{
-		return twoPair;
+		boolean check = false;
+		if(values[1] == values[2] && values[3] == values[4])
+			check = true;
+		if(values[2] == values[3] && values[4] == values[5])
+			check = true;
+		if(values[1] == values[2] && values[4] == values[5])
+			check = true;
+		return check;
 	}
-	private boolean isThreeOfKind()
+	private boolean isThreeOfKind(int[] values)
 	{
-		return threeOfKind;
+		boolean check = false;
+		if(values[1] == values[2] && values[1] == values[3])
+			check = true;
+		if(values[2] == values[3] && values[2] == values[4])
+			check = true;
+		if(values[3] == values[4] && values[3] == values[5])
+			check = true;
+		return check;
 	}
-	private boolean isFourOfKind()
+	private boolean isFourOfKind(int[] values)
 	{
-		return fourOfKind;
+		boolean check = false;
+		if(values[1] == values[2] && values[1] == values[3] && values[1] == values[4])
+			check = true;
+		if(values[2] == values[3] && values[2] == values[4] && values[2] == values[5])
+			check = true;
+		return check;
 	}
-	private boolean isStraight()
+	private boolean isStraight(int[] values)
 	{
-		return Straight;
+		boolean check = false;
+		if(values[1] == values[2]-1
+				&& values[2] == values[3]-1
+				&& values[3] == values[4]-1
+				&& values[4] == values[5]-1)
+			check = true;
+		return check;
 	}
 	private boolean isFlush()
 	{
-		return Flush;
+		boolean check = true;
+		String control = card1.getSuit();
+		if(!card2.getSuit().equals(control))
+			check = false;
+		if(!card3.getSuit().equals(control))
+			check = false;
+		if(!card4.getSuit().equals(control))
+			check = false;
+		if(!card5.getSuit().equals(control))
+			check = false;
+		return check;
 	}
-	private boolean isFullHouse()
+	private boolean isFullHouse(int[] values)
 	{
-		return true;
+		boolean check = false;
+		if(values[1] == values[2] && values[1] == values[3] && values[4] == values[5])
+			check = true;
+		if(values[2] == values[1] && values[3] == values[4] && values[3] == values[5])
+			check = true;
+		return check;
 	}
-	private boolean isStraightFlush()
+	private boolean isStraightFlush(int[] values)
 	{
-		return true;
+		boolean check = true;
+		String control = card1.getSuit();
+		if(!card2.getSuit().equals(control))
+			check = false;
+		if(!card3.getSuit().equals(control))
+			check = false;
+		if(!card4.getSuit().equals(control))
+			check = false;
+		if(!card5.getSuit().equals(control))
+			check = false;
+		if(check=true)
+			if(values[1] == values[2]-1
+			&& values[2] == values[3]-1
+			&& values[3] == values[4]-1
+			&& values[4] == values[5]-1)
+				check = true;
+			else
+				check = false;
+		return check;
 	}
-	private boolean isRoyalFlush()
+	private boolean isRoyalFlush(int[] values)
 	{
-		return true;
+		boolean check = true;
+		String control = card1.getSuit();
+		if(!card2.getSuit().equals(control))
+			check = false;
+		if(!card3.getSuit().equals(control))
+			check = false;
+		if(!card4.getSuit().equals(control))
+			check = false;
+		if(!card5.getSuit().equals(control))
+			check = false;
+		if(check=true)
+		if(values[1] == 1 && 
+				values[2] == 10 &&
+				values[3] == 11 &&
+				values[4] == 12 &&
+				values[5] == 13)
+					check = true;
+		else
+			check = false;
+		return check;
 	}
 }
 
