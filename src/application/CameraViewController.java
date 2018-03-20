946ff2e62fb62b41e8c3e0f22c8e8624c9ef1e09
JayVirta <31388873+JayVirta@users.noreply.github.com>
2018-03-19 20:04:55 -0400

    Fixed Camera
    Got camera feed working using code by Luigi De Russi

--- src/application/CameraViewController.java
+++ src/application/CameraViewController.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.awt.image.BufferedImage;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.awt.image.DataBufferByte;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -27,30 +31,136 @@ public class CameraViewController
 	@FXML
 	private ImageView currentFrame;
 	private boolean cameraActive = false;
+	private boolean reading = true;
 	private static int cameraId = 0;
+	VideoCapture capture;
 	@FXML
 	protected void startCamera(ActionEvent event)
 	{
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        VideoCapture camera = new VideoCapture(0);
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        if(!camera.isOpened()){
-            System.out.println("Error");
-        }
-        else {
-            Mat frame = new Mat();
-            camera.read(frame); 
-            while(true){
-                if (camera.read(frame)){
-                    System.out.println("Frame Obtained");
-                    System.out.println("Captured Frame Width " + 
-                    frame.width() + " Height " + frame.height());
-                    Imgcodecs.imwrite("camera.jpg", frame);
-                    System.out.println("OK");
-                    break;
-                }
-            }   
-        }
-        camera.release();
-    }
+		capture = new VideoCapture(cameraId);
+		if (!this.cameraActive)
+		{
+			// start the video capture
+			capture.open(cameraId);
+			
+			// is the video stream available?
+			if (capture.isOpened())
+			{
+				this.cameraActive = true;
+				
+				// grab a frame every 33 ms (30 frames/sec)
+				Runnable frameGrabber = new Runnable() {
+					
+					@Override
+					public void run()
+					{
+						// effectively grab and process a single frame
+						Mat frame = grabFrame();
+						// convert and show the frame
+						Image imageToShow = Utils.mat2Image(frame);
+						updateImageView(currentFrame, imageToShow);
+					}
+				};
+				
+				this.timer = Executors.newSingleThreadScheduledExecutor();
+				this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+				
+				// update the button content
+				this.startBtn.setText("Stop Camera");
+			}
+			else
+			{
+				// log the error
+				System.err.println("Impossible to open the camera connection...");
+			}
+		}
+		else
+		{
+			// the camera is not active at this point
+			this.cameraActive = false;
+			// update again the button content
+			this.startBtn.setText("Start Camera");
+			
+			// stop the timer
+			this.stopAcquisition();
+		}
+	}
+	private Mat grabFrame()
+	{
+		// init everything
+		Mat frame = new Mat();
+		
+		// check if the capture is open
+		if (this.capture.isOpened())
+		{
+			try
+			{
+				// read the current frame
+				this.capture.read(frame);
+				
+				// if the frame is not empty, process it
+				if (!frame.empty())
+				{
+					Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+				}
+				
+			}
+			catch (Exception e)
+			{
+				// log the error
+				System.err.println("Exception during the image elaboration: " + e);
+			}
+		}
+		
+		return frame;
+	}
+	private static BufferedImage matToBufferedImage(Mat original)
+	{
+		// init
+		BufferedImage image = null;
+		int width = original.width(), height = original.height(), channels = original.channels();
+		byte[] sourcePixels = new byte[width * height * channels];
+		original.get(0, 0, sourcePixels);
+		
+		if (original.channels() > 1)
+		{
+			image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+		}
+		else
+		{
+			image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+		}
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+		return image;
+	}
+	
+	private void updateImageView(ImageView view, Image image)
+	{
+		Utils.onFXThread(view.imageProperty(), image);
+	}
+	private void stopAcquisition()
+	{
+		if (this.timer!=null && !this.timer.isShutdown())
+		{
+			try
+			{
+				// stop the timer
+				this.timer.shutdown();
+				this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+			}
+			catch (InterruptedException e)
+			{
+				// log any exception
+				System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+			}
+		}
+		
+		if (this.capture.isOpened())
+		{
+			// release the camera
+			this.capture.release();
+		}
+	}
 	}
